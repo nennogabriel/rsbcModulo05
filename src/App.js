eb3ca25993c53c30056b5c4b077cee69642f41d6
Pedro Nascimento <nennogabriel@gmail.com>
2019-09-02 15:46:54 -0300

    05a05 global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Routes from './route';
+import GlobalStyle from './styles/global';
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
